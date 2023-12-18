c6886e98455ca062e13e0da04d1e1c0487ad237f
mugunthan s <mugunthan701@gmail.com>
2023-12-18 21:35:36 +0530

    completed

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,38 @@
-import React, { useState } from "react";
-import "./Carousel.css";
-import { images } from "../data/CarouselData";
+import React, { useState } from 'react';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { images } from '../data/CarouselData';
 
-// you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+const Carousel = () => {
+    const [id, setId] = useState(0);
 
-// complete the function below:
-function Carousel() {
+    const handlePrevClick = () => {
+        setId((prevId) => (prevId - 1 + images.length) % images.length);
+    };
+
+    const handleNextClick = () => {
+        setId((prevId) => (prevId + 1) % images.length);
+    };
+
+    const currImage = images[id];
+
+    return (
+        <div className="container">
+            <div className="carousel">
+                <div className="carousel-title">{currImage.title}</div>
+                <div className="carousel-subtitle">{currImage.subtitle}</div>
+            </div>
+            <div className="alignitems">
+                <div className="arrow-back" onClick={handlePrevClick}>
+                    <ArrowBackIosIcon sx={{ color: 'black' }} />
+                </div>
+                <img className="carousel-img" src={currImage.img} alt="" />
+                <div className="arrow-forward" onClick={handleNextClick}>
+                    <ArrowForwardIosIcon sx={{ color: 'black' }} />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Carousel;
--- src/data/CarouselData.jsx
+++ src/data/CarouselData.jsx
@@ -1,17 +1,19 @@
-import faces from "../images/faces.jpg";
-import girlface from "../images/girlface.jpg";
-import eyes from "../images/eyes.jpg";
+import React from 'react';
+import Carousel from '../components/Carousel'; // Update the path based on your project structure
+import faces from '../images/faces.jpg';
+import girlface from '../images/girlface.jpg';
+import eyes from '../images/eyes.jpg';
 
 export const images = [
-  { title: "Faces", subtitle: "How many faces do you have?", img: faces },
+  { title: 'Faces', subtitle: 'How many faces do you have?', img: faces },
   {
-    title: "Inside out",
-    subtitle: "The best person to guide you is yourself",
+    title: 'Inside out',
+    subtitle: 'The best person to guide you is yourself',
     img: girlface,
   },
   {
-    title: "Crying Lady",
-    subtitle: "Why so serious?",
+    title: 'Crying Lady',
+    subtitle: 'Why so serious?',
     img: eyes,
   },
 ];
